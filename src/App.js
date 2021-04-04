51531cc53b499ff9a798c0334831d63510f68b62
Arman Eskandari <arman_eskandari@live.com>
2021-04-04 23:51:56 +0430

    implemented Router and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 function App() {
   return (
-    <div className="">
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
