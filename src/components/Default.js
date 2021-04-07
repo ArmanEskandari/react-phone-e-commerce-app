9a60c7d3fbceaac6242a3219e0a67329a237a909
Arman Eskandari <arman_eskandari@live.com>
2021-04-07 12:48:44 +0430

    improved default(not found) page

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-export default function Default() {
+export default function Default(props) {
   return (
-    <div>
-      <h3>Page not found</h3>
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-uppercase pt-5">
+          <h1 className="display-3">404</h1>
+          <h1>error</h1>
+          <h2>page not found</h2>
+          <h4>
+            the requested url
+            <span className="text-danger">{props.location.pathname}</span> was
+            not found
+          </h4>
+        </div>
+      </div>
     </div>
   );
 }
